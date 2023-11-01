21bad2d0414a92700b32fdbab462757b24ae87bc
YukiMiyake <miyake-yu@mb.meidensha.co.jp>
2023-11-01 16:43:24 +0900

    開始地点、終了地点を追加

--- src/Grid.js
+++ src/Grid.js
@@ -1,15 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Grid = () => {
     const rows = Array.from(Array(10).keys());
     const cols = Array.from(Array(10).keys());
+
+    const [start, setStart] = useState(null);
+    const [goal, setGoal] = useState(null);
+
+    const handleClick = (row, col) => {
+        if (!start) {
+            setStart([row, col]);
+        } else if (!goal) {
+            setGoal([row, col]);
+        } else {
+            setStart([row, col]);
+            setGoal(null);
+        }
+    };
 
     return (
         <div style={{ display: 'flex', flexDirection: 'column' }}>
             {rows.map((row) => (
                 <div key={row} style={{ display: 'flex' }}>
                     {cols.map((col) => (
-                        <div key={col} style={{ width: 20, height: 20, border: '1px solid black' }} />
+                        <div
+                            key={col}
+                            style={{ width: 20, height: 20, border: '1px solid black', textAlign: 'center' }}
+                            onClick={() => handleClick(row, col)}
+                        >
+                            {start && start[0] === row && start[1] === col && 'S'}
+                            {goal && goal[0] === row && goal[1] === col && 'G'}
+                        </div>
                     ))}
                 </div>
             ))}
